68382ad5ff9466efb5f8066e936188fe00234df3
Alex Stokes <r.alex.stokes@gmail.com>
2023-11-25 08:06:20 -0700

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.simpleFlake {
+      inherit self nixpkgs;
+      name = "shell";
+      shell = ./shell.nix;
+    };
+}
